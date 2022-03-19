c850bb0022d91673bba7cbcd4887e7342f7e342e
Socheath Horl <socheathorl173@gmail.com>
2022-03-19 04:06:53 +0000

    Testing sync

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks. Testing Sync"""
 import os
 import sys
 
